2877acd925627798e341632dce4c582df303dbd9
nastu <nastushik92@bk.ru>
2022-08-27 22:05:22 +0300

    дз от 27.08

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int i = 0;
+        while ( int i = 0;
+        i < 10;
+        i++){
+            System.out.println("Вывести числа от 1 до 10 " + i);
+        }
+
     }
+
+    int i = 10;
+        while(int i = 10;i<0;i --){
+        System.out.println("Вывести числа от 10 до 1 " + i);
+    }
+
+    // задание 2
+    int a = 5;
+    for(int a = 5;i<31;i=i+5){
+        System.out.println("Сегодня пятница" + i"-е число. Необходимо подготовить отчет");
+    }
+// задание 3
+    int  b =1822;
+    while(int b= 1822; i<2122; i=i+79){
+        System.out.println("комета пролетит" + i + "году");
+    }
+
 }
